4b173853eee2e90f7b68cea874f49bd9dff7feeb
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-11 22:49:42 +0530

    storybook add and newscard add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.webp';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to National Institute of Technology, Patna
-        </p>
-        <a
-          className="App-link"
-          href="http://nitp.ac.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Old Website
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
