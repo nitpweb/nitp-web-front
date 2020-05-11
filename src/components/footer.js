45005ec3cdd41395486cedd8d0995199669fb565
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-12 01:26:26 +0530

    footer and important link add

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './footer.css';
+
+const Footer=()=>{
+    return (
+        <div className="foot-row">
+            <div className="foot-col-4">
+            <h1>Contact Us</h1>
+            <h3>National Institute of Technology Patna</h3>
+            <h3> Patna, Bihar (800005), India</h3>
+            <h3> Phone: +91-0612-237 1715 / 237 2715</h3>
+            <h3>FAX : +91-0612-2670631 , 0612-2660480</h3>
+            </div>
+            <div className="foot-col-8"></div>
+        </div>
+    )
+}
+export default Footer;
--- src/components/home.js
+++ src/components/home.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import Newscard from './home/newscard';
-import chanakya from './home/img/chanakya.svg';
-import ieee from './home/img/ieee.svg';
-import asme from './home/img/asme.svg';
-import corona from './home/img/corona.svg';
-import josaa from './home/img/josaa.svg';
-import teqip from './home/img/teqip.svg';
-import nss from './home/img/nss.svg';
-import './home/css/home.css';
 
+import './home/css/home.css';
+import Importantlink from './home/importantlink';
+import nitpimg from './home/img/nitp.svg';
 
 
 const Home=()=>{
     return (
         <body>
-            <div className="link-row">
-                <div className="link-col">
-                    <img className="implink" src={chanakya} alt="chanakya"></img>
-                    <img className="implink" src={asme} alt="asme"></img>
-                    <img className="implink" src={corona} alt="corona"></img>
-                    <img className="implink" src={ieee} alt="ieee"></img>
-                </div>
-                <div className="link-col">
-                    <img className="implink" src={josaa} alt="josaa"></img>
-                    <img className="implink" src={teqip} alt="teqip"></img>
-                    <img className="implink" src={nss} alt="nss"></img>
-                    <img className="implink" src={chanakya} alt="chanakya"></img>
-
-                </div>
+            <div>
+                <div className="bgimgwrap"><img className="nitpbackimg" src={nitpimg} alt="NATIONAL INSTITUTE OF TECHNOLOGY PATNA" /></div>
             </div>
+            <Importantlink />            
             <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
         </body>
     )
