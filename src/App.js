527408bb1b2919ae50d424abb5a581d089728f71
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-12 01:26:26 +0530

    footer and important link add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './components/home';
+import Footer from './components/footer';
+
 
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Home />
+      <Footer />
     </div>
   );
 }
--- src/components/home/eventcard.js
+++ src/components/home/eventcard.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import cal from './img/calendar.svg'
+
+const Eventcard=(info)=>{
+    return(
+        <div className="card">
+            <img src={cal} alt="calendar" />
+        </div>
+    )
+}
+export default Eventcard;
