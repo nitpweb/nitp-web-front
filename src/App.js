5feb19c642f8d7974af56dc1089b528030855e5a
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-12 16:04:35 +0530

    navbar logo and fonts add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/home';
 import Footer from './components/footer';
+import Navbar from './components/navbar';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <Footer />
     </div>
