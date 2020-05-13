916c61ebc480a66226cb3a2df80c16ef566853f2
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-13 15:04:40 +0530

    navbar update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,6 +10,7 @@ const Navbar=()=>{
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.querySelector(".nav-col>h2").style.lineHeight = "0.3rem";
       document.querySelector(".nav-col>h4").style.lineHeight = "0.3rem";
+      document.querySelector(".nav-col").style.paddingTop = "0.5vh";
       document.querySelector(".nav-row").style.height = "8vh";
       document.querySelector(".logobadge>h4").style.display = "none";
       document.querySelector(".logobadge>img").style.maxHeight= "8vh";
@@ -22,10 +23,18 @@ const Navbar=()=>{
       document.querySelector(".logobadge>h4").style.transition = "1s";
       document.querySelector(".logobadge>img").style.transition = "1s";
       document.querySelector(".mobilelogo>img").style.transition = "1s";
+      document.querySelector(".nav-link-row").style.height = "4vh";
+      document.querySelector(".nav-link-row").style.marginTop = "8vh";
+      document.querySelector(".nav-link-row").style.backgroundColor = "#F7DDB5";
+      document.querySelector(".nav-link-row").style.opacity = "1";
+
     } else {
       document.querySelector(".nav-col>h2").style.lineHeight = "0.6rem";
       document.querySelector(".nav-col>h4").style.lineHeight = "0.6rem";
       document.querySelector(".nav-row").style.height = "10vh";
+      document.querySelector(".nav-link-row").style.height = "7vh";
+      document.querySelector(".nav-link-row").style.backgroundColor = "black";
+      document.querySelector(".nav-link-row").style.marginTop = "10vh";
       document.querySelector(".logobadge>h4").style.display = "block";
       document.querySelector(".logobadge>img").style.maxHeight = "15vh";
       document.querySelector(".logobadge>img").style.maxHeight= "15vh";
@@ -38,10 +47,14 @@ const Navbar=()=>{
       document.querySelector(".logobadge>h4").style.transition = "1s";
       document.querySelector(".logobadge>img").style.transition = "1s";
       document.querySelector(".mobilelogo>img").style.transition = "1s";
+      document.querySelector(".nav-link-row").style.opacity = "0.5";
+      document.querySelector(".nav-col").style.paddingTop = "1vh";
+
 
     }
   } 
     return (
+      <>
       <div className="nav-row">
         <div className="mobilelogo">
           <img src={logo} alt="NIT PATNA" />
@@ -50,12 +63,15 @@ const Navbar=()=>{
           <h2>NATIONAL INSTITUTE OF TECHNOLOGY PATNA</h2>
           <h4>राष्ट्रीय प्रौद्योगिकी संस्थान, पटना</h4>
         </div>
+          <div className="nav-link-row"></div>
+
         <div className="logobadge">
           <h4>श्रमोऽनवरत चेष्टाय</h4>
           <img src={logo} alt="NIT PATNA" />
         </div>
         <button className="nav-toggle"><img src={toggle} alt="menu" /></button>
       </div>
+      </>
     );
 }
 
