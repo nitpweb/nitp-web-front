1e545a5212981e508ac115056e08f000d800638b
Divya Prakash <medivyaprakash@gmail.com>
2020-05-24 22:19:40 +0530

    navbar update

--- src/components/home.js
+++ src/components/home.js
@@ -13,6 +13,18 @@ const Home=()=>{
                 <div className="bgimgwrap"><img className="nitpbackimg" src={nitpimg} alt="NATIONAL INSTITUTE OF TECHNOLOGY PATNA" /></div>
             </div>
             <Importantlink />
+            <div className="news-row">
+                <div className="news-viewbox">
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                    <Newscard head="Workshop organised" detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.." />
+                </div>
+            </div>
             <div className="news-head">
                 News
                 <p>view all</p>
