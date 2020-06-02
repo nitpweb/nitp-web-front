eaba3bebe42ac077fbffeb86acf77b3d62ec242e
Divya Prakash <medivyaprakash@gmail.com>
2020-06-02 21:28:14 +0530

    notice card complete

--- src/components/home/eventcard.js
+++ src/components/home/eventcard.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import cal from './img/calendar.svg'
+import cal from './img/calendar.svg';
+import {css} from 'styled-components';
 
-const Eventcard=(info)=>{
+const Eventcard=(props)=>{
     return(
-        <div className="card">
+        <div css={css`width:93%;padding:10px;background: #FFFFFF;box-shadow: 0px 0px 26px rgba(207, 207, 207, 0.36);border-radius: 20px;margin-top:20px;display:flex`}>
             <img src={cal} alt="calendar" />
+            <h4 css={css`margin-left:5%`}>{props.detail}</h4>
         </div>
     )
 }
