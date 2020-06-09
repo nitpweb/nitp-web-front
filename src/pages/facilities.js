319b9c9289e00fbdd10e6f64de02a0d80da1562a
Divya Prakash <medivyaprakash@gmail.com>
2020-06-09 19:22:34 +0530

    facilities page

--- src/pages/facilities.js
+++ src/pages/facilities.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Facilitiespage from '../components/facilities';
 
 const Facilities = () => (
     <Layout>
         <SEO title="Students" />
-        <h1>HI</h1>
+        <Facilitiespage/>
     </Layout>
 )
 
