483a5bc75eae0ecf72a2bd4e550ef8a20d3b03ed
Divya Prakash <medivyaprakash@gmail.com>
2020-06-17 18:02:32 +0530

    all inline styles to external css added

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"national-institute-of-technology-patna","short_name":"nitpatna","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/logo512.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"48e913bde2323ac62a1dab5ffae7d2c5"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
