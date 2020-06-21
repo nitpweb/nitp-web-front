66b438c18be92f0965e6baec5e49e7035b356daa
Divya Prakash <medivyaprakash@gmail.com>
2020-06-21 15:39:52 +0530

    update color

--- src/components/academics.js
+++ src/components/academics.js
@@ -5,22 +5,23 @@ import paper from "./academics/img/paper.svg"
 import test from "./academics/img/test.svg"
 import "./academics/css/academics.css"
 import Acadnotice from "./academics/acadnotice"
+import {Acadnoticeb} from "./academics/acadnotice"
 
 const Academicspage = () => {
   return (
-    <div className="depsection">
-      <div className="dmainrow dmainrowmain">
+    <div className="acadsection">
+      <div className="acmainrow acmainrowmain">
         <div className="col-6">
-          <div className="row">
+          <div className="row rowmarl3">
             <h1>Academics</h1>
           </div>
-          <div className="row">
+          <div className="row rowmarl3">
             <h2>
               “Education is not the learning of facts, but the training of minds
               to think”
             </h2>
           </div>
-          <div className="row">
+          <div className="row rowmarl3">
             <h2 id="aut">- Albert Einstein</h2>
           </div>
         </div>
@@ -28,7 +29,7 @@ const Academicspage = () => {
           <img src={acadscholar} className="img-fluid" loading="lazy" />
         </div>
       </div>
-      <div className="dmainrow dmainrow1">
+      <div className="acmainrow acmainrow1">
         <div className="col-6 imgcolstyle">
           <img src={test} className="img-fluid" loading="lazy" />
         </div>
@@ -38,22 +39,22 @@ const Academicspage = () => {
           </div>
           <div className="row rowmarr3">
             <div>
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="22 June,2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="21 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="20 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="20 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
@@ -62,12 +63,12 @@ const Academicspage = () => {
           </div>
         </div>
       </div>
-      <div className="dmainrow">
+      <div className="acmainrow">
         <div className="col-6">
-          <div className="row">
+          <div className="row rowmarl3">
             <h1>Formats</h1>
           </div>
-          <div className="row">
+          <div className="row rowmarl3">
             <div>
               <Acadnotice
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
@@ -96,32 +97,32 @@ const Academicspage = () => {
           <img src={paper} className="img-fluid" loading="lazy" />
         </div>
       </div>
-      <div className="dmainrow dmainrow1">
+      <div className="acmainrow acmainrow1">
         <div className="col-6 imgcolstyle">
           <img src={mail} className="img-fluid" loading="lazy" />
         </div>
-        <div className="col-6">
+        <div className="col-6 imgcolstyle">
           <div className="row">
             <h1>Notice</h1>
           </div>
           <div className="row rowmarr3">
             <div>
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="22 June,2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="21 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="20 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
               />
-              <Acadnotice
+              <Acadnoticeb
                 detail="Online Next-Generation Semiconductor Devices for high-end applications"
                 time="20 June 2020"
                 url="http://www.nitp.ac.in/uploads/Online_FDP_Nextgen_Devices.pdf"
