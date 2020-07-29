35d3b6e40a336a7fc9cedc37c80fb1283799fa5c
Divya Prakash <medivyaprakash@gmail.com>
2020-07-29 14:07:33 +0530

    administration page

---
+++ src/components/administration/adcard.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./css/adcard.css"
+
+const Adcard = props => {
+  return (
+    <>
+      <div className="adcard">
+        <div className="row adrow adname">
+          <span>{props.name}</span>
+        </div>
+        <div className="row adrow designation">
+          <span>{props.designation}</span>
+        </div>
+        <div className="row type">
+          <span>{props.type}</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Adcard
---
+++ src/components/administration/const.js
@@ -0,0 +1,207 @@
+import React from "react"
+
+export const Deansinfo = [
+  {
+    name: "Prof. Pradip Kumar Jain",
+    designation: "Chairman, Board of Governors",
+    type: "Chairman",
+  },
+  {
+    name: "Prof. S.K. Verma",
+    designation: "Deputy Director and Dean, Academic",
+    type: "Dean",
+  },
+  {
+    name: "Prof. Om Prakash",
+    designation: "Dean, Faculty Welfare",
+    type: "Dean",
+  },
+  {
+    name: "Prof. Prakash Chandra",
+    designation: "Dean, Student Welfare",
+    type: "Dean",
+  },
+  {
+    name: "Prof. Fulena Rajak",
+    designation: "Dean, Planning and Development",
+    type: "Dean",
+  },
+  {
+    name: "Prof. Lal Bahadur Roy",
+    designation: "Dean, Research and consultancy",
+    type: "Dean",
+  },
+  {
+    name: "Dr. Asit Narayan",
+    designation: "Registrar, NIT Patna",
+    type: "Registrar",
+  },
+]
+
+export const BOD = [
+  {
+    name: "Prof. Pradip Kumar Jain",
+    designation: "Chairperson(Actg.) BOG, Ex-Officio Member, NIT Patna",
+    type: "chairperson",
+  },
+  {
+    name: "Dr. Asit Narayan",
+    designation: "Registrar, NIT Patna",
+    type: "Secretary",
+  },
+  {
+    name: "Joint Secretary(Technical) or his/her nominee",
+    designation: "Department of Higher Education, MHRD",
+    type: "Member",
+  },
+  {
+    name: "JS and Financial Advisor or his/her nominee",
+    designation: "Department of Higher Education, MHRD",
+    type: "Member",
+  },
+  {
+    name: "Sri Shailendra P. Sinha",
+    designation:
+      "Managing Director Kalyanpur Cement Ltd.,Maurya Center,1 , Frazer Road, PB No. 89, Patna-800001(Bihar)",
+    type: "Member",
+  },
+  {
+    name: "Sri Aditya Prakash Mishra",
+    designation: "Former Member, Railway Engineering Board",
+    type: "Member",
+  },
+  {
+    name: "Dr. Chandrawati Jee",
+    designation:
+      "Former Head, P.G. Department of Biotechnology, A.N. College, Patna",
+    type: "Member",
+  },
+  {
+    name: "Dr. Narendra Kumar Singh",
+    designation:
+      "Associate Professor & Head,Department of Physics, S.N. Sinha College, Jehanabad",
+    type: "Member",
+  },
+  {
+    name: "Prof. S.K. Verma",
+    designation: "Professor, Mechanical Engg. Dept., NIT Patna",
+    type: "Member",
+  },
+  {
+    name: "Director IIT Patna",
+    designation: "or his nominee not below the rank of professor",
+    type: "Member",
+  },
+]
+
+export const BWC = [
+  {
+    name: "Prof. Pradip Kumar Jain",
+    designation: "Director, NIT Patna",
+    type: "Ex-Officio Chairperson",
+  },
+  {
+    name: "Director or Deputy Secretary",
+    designation: "or his nominee Dealing with NITs in Ministry",
+    type: "Ex-Officio Member",
+  },
+  {
+    name: "Director or Deputy Secretary",
+    designation: "or his nominee Dealing with Finance of NITs in Ministry",
+    type: "Ex-Officio Member",
+  },
+  {
+    name: "Dr. Rajesh Kumar",
+    designation: "Professor Department of Civil Engineering IIT BHU",
+    type: "Member",
+  },
+  {
+    name: "Sri Ashok Kumar",
+    designation:
+      "Director (Operation) South Bihar Power Distribution Co. Ltd Patna",
+    type: "Member",
+  },
+  {
+    name: "Sri Pawan Kumar",
+    designation: `Executive Engineer
+Construction Divison-1
+BCD, Govt. of Bihar`,
+    type: "Member",
+  },
+  {
+    name: `Prof. Fulena Rajak`,
+    designation: `Dean (Planning & Development), NIT Patna`,
+    type: `Member`,
+  },
+  {
+    name: `Dr. Asit Narayan`,
+    designation: `Registrar, NIT Patna`,
+    type: `Secretary`,
+  },
+]
+
+export const IDC = [
+  {
+    name: "Prof. Prakash Chandra",
+    designation: "Dean(Student Welfare)",
+    type: "Chairman",
+  },
+  {
+    name: "Prof. Vivekanand Singh",
+    designation: "Director's Nominee",
+    type: "Member",
+  },
+  {
+    name: "Dr. Samrat Mukherjee",
+    designation: "Chairman Hostel Management Committee",
+    type: "Member",
+  },
+  {
+    name: "",
+    designation:
+      "Warden of the Hostel of Residence of which the student concerned is a boarder",
+    type: "Member",
+  },
+  {
+    name: "Dr. T. Roshni",
+    designation: "One faculty nominated by the Senate",
+    type: "Member",
+  },
+  {
+    name: "",
+    designation: "Students Representative nominated by Dean (Student Welfare)",
+    type: "Member",
+  },
+  {
+    name: "Dr. Asit Narayan",
+    designation: "Registrar, NIT Patna",
+    type: "Member Secretary",
+  },
+]
+export const FC = [
+  {
+    name: "Prof. Pradip Kumar Jain",
+    designation: "Director, NIT Patna",
+    type: "Chairperson",
+  },
+  {
+    name: "Joint Secretary(Technical) or his/her nominee",
+    designation: "Department of Higher Education,MHRD",
+    type: "Member",
+  },
+  {
+    name: "JS and FInancial Advisor",
+    designation: "Department of Higher Education, MHRD",
+    type: "Member",
+  },
+  {
+    name: "Director IIT Patna",
+    designation: "or his nominee not below the rank of professor",
+    type: "Member",
+  },
+  {
+    name: "Dr. Asit Narayan",
+    designation: "Registrar, NIT Patna",
+    type: "Secretary",
+  },
+]
