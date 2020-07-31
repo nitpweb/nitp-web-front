e5a85a806472459febc836a3c8e6c6fdbb13bdde
Divya Prakash <medivyaprakash@gmail.com>
2020-07-31 22:43:05 +0530

    noticelist add

--- src/components/home/notice.js
+++ src/components/home/notice.js
@@ -1,12 +1,22 @@
 import React from "react"
 import "./css/notice.css"
-
+import downimg from "./img/download.svg"
 const Notice = props => {
   return (
     <>
       <div className="noticecard">
         <p className="noticecarddetail">{props.detail}</p>
-        <p id="fp">{props.time}</p>
+        <div id="notdate">
+          <div>
+            <p className="notet">{props.time}</p>
+          </div>
+          <div>
+            <img id="notdownimg" src={downimg} alt="c" />
+            <a className="notdown" href={props.url} target="blank">
+              Download
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
