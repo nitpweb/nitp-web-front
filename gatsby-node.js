9de4f3570f380dc8428a4c6db69de94caba485af
Divya Prakash <medivyaprakash@gmail.com>
2020-09-26 16:57:34 +0530

    test update

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,41 +45,44 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
   const eventdummynode = Object.assign({}, eventdummy, eventdummyMeta)
   createNode(eventdummynode)
   const fetchNotice = () => {
-    axios.get(`https://nitpadmin.herokuapp.com/api/notice/active`).then(res => {
-      // map into these results and create nodes
-      res.data.map((notice, i) => {
-        // Create your node object
-        const noticeNode = {
-          // Required fields
-          id: `${i}`,
-          parent: `__SOURCE__`,
-          internal: {
-            type: `Notice`, // name of the graphQL query --> allRandomUser {}
-            // contentDigest will be added just after
-            // but it is required
-          },
-          children: [],
+    axios
+      .get(`https://nitpadmin.herokuapp.com/api/notice/active`)
+      .then(res => {
+        // map into these results and create nodes
+        res.data.map((notice, i) => {
+          // Create your node object
+          const noticeNode = {
+            // Required fields
+            id: `${i}`,
+            parent: `__SOURCE__`,
+            internal: {
+              type: `Notice`, // name of the graphQL query --> allRandomUser {}
+              // contentDigest will be added just after
+              // but it is required
+            },
+            children: [],
 
-          // Other fields that you want to query with graphQl
-          title: notice.title,
-          timestamp: notice.timestamp,
-          important: notice.important,
-          attachments: notice.attachments,
-          // etc...
-        }
+            // Other fields that you want to query with graphQl
+            title: notice.title,
+            timestamp: notice.timestamp,
+            important: notice.important,
+            attachments: notice.attachments,
+            // etc...
+          }
 
-        // Get content digest of node. (Required field)
-        const contentDigest = crypto
-          .createHash(`md5`)
-          .update(JSON.stringify(noticeNode))
-          .digest(`hex`)
-        // add it to userNode
-        noticeNode.internal.contentDigest = contentDigest
+          // Get content digest of node. (Required field)
+          const contentDigest = crypto
+            .createHash(`md5`)
+            .update(JSON.stringify(noticeNode))
+            .digest(`hex`)
+          // add it to userNode
+          noticeNode.internal.contentDigest = contentDigest
 
-        // Create node with the gatsby createNode() API
-        createNode(noticeNode)
+          // Create node with the gatsby createNode() API
+          createNode(noticeNode)
+        })
       })
-    })
+      .catch(e => console.log(e))
   }
   const fetchEvents = () => {
     axios.get(`https://nitpadmin.herokuapp.com/api/event`).then(res => {
@@ -115,7 +118,7 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
         // Create node with the gatsby createNode() API
         createNode(eventNode)
       })
-    })
+    }).catch(e=>console.log(e))
   }
   return Promise.all([fetchNotice(), fetchEvents()])
 }
