e2047b069a5ab3130077827914efaef261da6fa4
Divya Prakash <medivyaprakash@gmail.com>
2020-10-09 21:44:30 +0530

    api update

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import React from "react"
 import Newscard from "./home/newscard"
 import "./home/css/home.css"
@@ -8,167 +9,167 @@ import { Nitpbackimg } from "./home/nitpimg"
 import { Noticelist } from "./home/noticelist"
 import { graphql, useStaticQuery } from "gatsby"
 
-const Home = () => {
-  const data = useStaticQuery(graphql`
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       events: [],
+       notices: [],
+     }
+  }
+ 
+
+  
+  componentDidMount(){
+    let eventsUrl=`https://nitpadmin.herokuapp.com/api/event`;
+    let noticesUrl = `https://nitpadmin.herokuapp.com/api/notice`;
+    axios.get(eventsUrl).then(res=>
     {
-      allNotice {
-        edges {
-          node {
-            attachments {
-              caption
-              url
-            }
-            timestamp
-            title
-            id
-          }
-        }
-      }
-      allEvent {
-        edges {
-          node {
-            id
-            timestamp
-            title
-            attachments {
-              caption
-              url
-            }
-          }
-        }
-      }
-    }
-  `)
+      const event=res.data;
+      this.setState({events:event});
+      console.log(event)
+    }).catch(e=>{console.log(e)});
+    axios
+      .get(noticesUrl)
+      .then(res => {
+        const notice = res.data
+        this.setState({ notices: notice })
+        console.log(notice);
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 
-  return (
-    <div>
+  render() {
+    return (
       <div>
-        <div className="bgimgwrap">
-          <Nitpbackimg />
-        </div>
-      </div>
-      <div className="row tablinkcover">
-        <div className="col-4 tablink">
-          <a href="#notice">Notice</a>
-        </div>
-        <div className="col-4 tablink">
-          <a href="#events">Events</a>
-        </div>
-        <div className="col-4 tablink">
-          <a href="#news">News</a>
-        </div>
-      </div>
-      <Importantlink />
-      <div className="noticewrap" id="notice">
         <div>
-          <div
-            data-aos="zoom-in"
-            data-aos-duration="200"
-            className="notice-head"
-          >
-            Notice
-            <p>view all</p>
+          <div className="bgimgwrap">
+            <Nitpbackimg />
           </div>
-          <div className="notice-row" data-aos="fade-up">
-            {data.allNotice.edges.map((notice, i) => {
-              const item = notice.node
-              const newtime = new Date().getTime()
+        </div>
+        <div className="row tablinkcover">
+          <div className="col-4 tablink">
+            <a href="#notice">Notice</a>
+          </div>
+          <div className="col-4 tablink">
+            <a href="#events">Events</a>
+          </div>
+          <div className="col-4 tablink">
+            <a href="#news">News</a>
+          </div>
+        </div>
+        <Importantlink />
+        <div className="noticewrap" id="notice">
+          <div>
+            <div
+              data-aos="zoom-in"
+              data-aos-duration="200"
+              className="notice-head"
+            >
+              Notice
+              <p>view all</p>
+            </div>
+            <div className="notice-row" data-aos="fade-up">
+              {this.state.notices.map((notice) => {
+                const newtime = new Date().getTime()
 
-              const d = new Date(newtime - item.timestamp).getHours()
-              if (item.title != "") {
-                return (
-                  <Notice
-                    detail={item.title}
-                    time={d}
-                    attachments={item.attachments}
-                  />
-                )
-              }
-            })}
+                const d = new Date(newtime - notice.timestamp).getHours()
+                if (notice.title != "") {
+                  return (
+                    <Notice
+                      detail={notice.title}
+                      time={d}
+                      attachments={notice.attachments}
+                    />
+                  )
+                }
+              })}
+            </div>
+          </div>
+          <div id="events">
+            <div
+              data-aos="zoom-in"
+              data-aos-duration="200"
+              className="event-head"
+            >
+              Events
+              <p>view all</p>
+            </div>
+            <div className="event-row" data-aos="fade-up">
+              {this.state.events.map((event) => {
+                const date = new Date(event.timestamp)
+                const day = date.getDate()
+                const month = date.getMonth()
+                const year = date.getFullYear()
+                const monthname = date
+                  .toLocaleString("default", { month: "short" })
+                  .toUpperCase()
+                if (event.title != "") {
+                  return (
+                    <Eventcard
+                      detail={event.title}
+                      time={`${day}-${month}-${year}`}
+                      date={day}
+                      month={monthname}
+                      attachments={event.attachments}
+                    />
+                  )
+                }
+              })}
+            </div>
           </div>
         </div>
-        <div id="events">
-          <div
-            data-aos="zoom-in"
-            data-aos-duration="200"
-            className="event-head"
-          >
-            Events
-            <p>view all</p>
-          </div>
-          <div className="event-row" data-aos="fade-up">
-            {data.allEvent.edges.map((event, i) => {
-              const item = event.node
-              const date = new Date(item.timestamp)
-              const day = date.getDate()
-              const month = date.getMonth()
-              const year = date.getFullYear()
-              const monthname = date
-                .toLocaleString("default", { month: "short" })
-                .toUpperCase()
-              if (item.title != "") {
-                return (
-                  <Eventcard
-                    detail={item.title}
-                    time={`${day}-${month}-${year}`}
-                    date={day}
-                    month={monthname}
-                    attachments={item.attachments}
-                  />
-                )
-              }
-            })}
-          </div>
+        <div
+          data-aos="zoom-in"
+          data-aos-duration="200"
+          className="news-head"
+          id="news"
+        >
+          News
+          <p>view all</p>
         </div>
-      </div>
-      <div
-        data-aos="zoom-in"
-        data-aos-duration="200"
-        className="news-head"
-        id="news"
-      >
-        News
-        <p>view all</p>
-      </div>
 
-      <div className="news-row">
-        <div className="news-viewbox">
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
-          <Newscard
-            head="Workshop organised"
-            detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
-          />
+        <div className="news-row">
+          <div className="news-viewbox">
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+            <Newscard
+              head="Workshop organised"
+              detail="IEEE organised a Workshop for the students of 1st and 2nd year on the grand.."
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
