a5077261db5dfe51f1dd66f80d2cf2eeaa3b4d0d
Divya Prakash <medivyaprakash@gmail.com>
2020-10-22 02:31:50 +0530

    arch faculty

--- src/components/departments/arch/archsyllabus.js
+++ src/components/departments/arch/archsyllabus.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Table from "../../table"
 import Course from "./Architecture CourseStructure"
-import main from "../img/main.png"
+import main from "../img/book.svg"
 
 const Archsyllabus = props => {
   return (
