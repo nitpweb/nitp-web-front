c1da5569384ded37eed8a3c767edf3a544e7c819
Divya Prakash <medivyaprakash@gmail.com>
2020-10-22 02:33:11 +0530

    ece faculty

---
+++ src/components/departments/ece/ecefaculty.js
@@ -0,0 +1,73 @@
+import axios from "axios"
+import React from "react"
+import "../../global/css/faculty.css"
+import Facultycard from "../../global/facultycard"
+import faculty from "../img/faculty.svg"
+
+class Ecefacultypage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      faculties: [],
+    }
+  }
+  componentDidMount() {
+    let facultiesUrl = `http://beta.nitp.ac.in:3000/api/faculty/ece`
+    axios
+      .get(facultiesUrl)
+      .then(res => {
+        const faculty = res.data.faculties
+        this.setState({ faculties: faculty })
+        console.log(this.state.faculties)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  render() {
+    let domainUrl = `http://beta.nitp.ac.in:3000`
+
+    return (
+      <>
+        <div style={{ marginTop: `10vh`, marginBottom: `15vh` }}>
+          <div className="dmainrow dmainrowmain">
+            <div className="col-6">
+              <div className="row rowmarl3">
+                <h1 data-aos="zoom-in-right">Faculties</h1>
+              </div>
+              <div className="row rowmarl3">
+                <h1 data-aos="zoom-in-right">
+                  -Electronics and Communication Engineering
+                </h1>
+              </div>
+            </div>
+            <div className="col-6 imgcolstyle">
+              <img
+                data-aos="zoom-in"
+                src={faculty}
+                className="img-fluid"
+                style={{ maxHeight: `50vh` }}
+                loading="lazy"
+              />
+            </div>
+          </div>
+          <div className="row facultyrow">
+            {this.state.faculties.map(faculty => {
+              return (
+                <Facultycard
+                  name={faculty.name}
+                  email={faculty.email}
+                  extn={faculty.ext_no}
+                  research={faculty.research_interest}
+                  image={`${domainUrl}/${faculty.imgUrl}`}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+export default Ecefacultypage
