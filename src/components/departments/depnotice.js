0cd8f39f73fcb2618c713bdda79831411052f45a
Divya Prakash <medivyaprakash@gmail.com>
2020-10-22 20:50:07 +0530

    notice and float menu add

---
+++ src/components/departments/depnotice.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./css/depnotice.css"
+import downimg from "./img/download.svg"
+
+const Depnotice = props => {
+  return (
+    <>
+      <div className="depnoticecard">
+        <p className="depcarddetail">{props.detail}</p>
+        <div id="depnotdate">
+          <div>
+            <p className="dept">{props.time}</p>
+          </div>
+          <div>
+            {props.attachments.map(elem => {
+              return (
+                <>
+                  <img id="depdownimg" src={downimg} alt="c" />
+                  <a className="depdown" href={elem.url} target="blank">
+                    {elem.caption}
+                  </a>
+                </>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Depnotice
