963638f37526ec5c42d8dc35ac836b5b3e8a450a
Divya Prakash <medivyaprakash@gmail.com>
2020-10-22 20:49:50 +0530

    physics update

--- src/components/departments/physics/physicshome.js
+++ src/components/departments/physics/physicshome.js
@@ -4,152 +4,221 @@ import mountain from "../img/mountain.svg"
 import map from "../img/map.svg"
 import lens from "../img/lens.svg"
 import "../css/home.css"
+import Floatmenu from "../floatmenu"
+import Depnotice from "../depnotice"
+import pc from "../img/pc.svg"
 
-const Physicshomepage = () => {
-  return (
-    <div className="depsection">
-      <div className="dmainrow dmainrowmain">
-        <div className="col-6">
-          <div className="row rowmarl3">
-            <h1 data-aos="zoom-in-right">Physics Department</h1>
+import axios from "axios"
+class Physicshomepage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notices: [],
+    }
+  }
+  componentDidMount() {
+    let noticesUrl = `http://beta.nitp.ac.in:3000/api/notice`
+    axios
+      .get(noticesUrl)
+      .then(res => {
+        const notice = res.data
+        this.setState({ notices: notice })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+  render() {
+    return (
+      <div className="depsection">
+        <Floatmenu />
+        <div className="dmainrow dmainrowmain">
+          <div className="col-6">
+            <div className="row rowmarl3">
+              <h1 data-aos="zoom-in-right">Physics Department</h1>
+            </div>
+            <div className="row rowmarl3">
+              <h2 data-aos="zoom-in-right">
+                “The computer was born to solve problems that did not exist
+                before.”
+              </h2>
+            </div>
+            <div className="row rowmarl3">
+              <h2 data-aos="zoom-in-right" id="aut">
+                — Bill Gates
+              </h2>
+            </div>
           </div>
-          <div className="row rowmarl3">
-            <h2 data-aos="zoom-in-right">
-              “The computer was born to solve problems that did not exist
-              before.”
-            </h2>
-          </div>
-          <div className="row rowmarl3">
-            <h2 data-aos="zoom-in-right" id="aut">
-              — Bill Gates
-            </h2>
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={main}
+              className="img-fluid"
+              loading="lazy"
+            />
           </div>
         </div>
-        <div className="col-6 imgcolstyle">
-          <img
-            data-aos="zoom-in"
-            src={main}
-            className="img-fluid"
-            loading="lazy"
-          />
+        <div className="dmainrow" id="about">
+          <div className="depabout">
+            <div className="row">
+              <h1 data-aos="zoom-in">About</h1>
+            </div>
+            <div className="row">
+              <p data-aos="zoom-in">
+                The Department of Physics was established in 1924 and since then
+                has been functioning as an important part of the institute. As
+                of now, the Department has eight faculty members and four
+                support staffs at different levels.
+                <br />
+                <br />
+                At present, there are 12 students enrolled in Integrated M. Sc.
+                And 8 students enrolled in the Ph. D. program.
+                <br />
+                <br />
+                The Department is mentored by Prof. K.L.Chopra ,Ex-Director,IIT
+                Kharagpur.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="dmainrow" id="happening">
+          <div className="col-6">
+            <div className="row rowmarl3">
+              <h1 data-aos="zoom-in-right">Happenings</h1>
+            </div>
+            <div className="row rowmarl3">
+              <div id="depnoticewrap">
+                {this.state.notices.map(notice => {
+                  const newtime = new Date().getTime()
+
+                  let d = Math.round((newtime - notice.timestamp) / 3600000)
+                  if (d > 24) {
+                    d = `${Math.round(d / 24)} days ago`
+                  } else if (d < 1) {
+                    d = `Just now`
+                  } else if (d < 2) {
+                    d = `${d} hour ago`
+                  } else {
+                    d = `${d} hours ago`
+                  }
+                  if (notice.title != "") {
+                    return (
+                      <Depnotice
+                        detail={notice.title}
+                        time={d}
+                        attachments={notice.attachments}
+                      />
+                    )
+                  }
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={pc}
+              className="img-fluid"
+              loading="lazy"
+            />
+          </div>
+        </div>
+
+        <div className="dmainrow dmainrow1" id="mission">
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={mountain}
+              className="img-fluid"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-6">
+            <div className="row rowmarr3">
+              <h1 data-aos="zoom-in-left">Mission</h1>
+            </div>
+            <div className="row rowmarr3">
+              <p data-aos="zoom-in-left">
+                To train the students to be lifelong learners who will
+                contribute to the creation of new knowledge, new technology, and
+                innovation through excellence in research in emerging areas and
+                disciplines.
+                <br />
+                <br />
+                To educate students to be future leaders in science, technology,
+                industry, education and other professions and succeed in a
+                globally competitive environment.
+                <br />
+                <br />
+                To create National and International collaborations for research
+                engagement in strategic areas of research
+                <br />
+                <br />
+                To create National and International collaborations for research
+                engagement in strategic areas of research
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="dmainrow" id="vision">
+          <div className="col-6">
+            <div className="row rowmarl3">
+              <h1 data-aos="zoom-in-right">Vision</h1>
+            </div>
+            <div className="row rowmarl3">
+              <p data-aos="zoom-in-right">
+                The vision of the Department is to achieve excellence in
+                undergraduate and postgraduate education and research through a
+                synergic combination and thereby lead to development of new
+                knowledge.
+              </p>
+            </div>
+          </div>
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={lens}
+              className="img-fluid"
+              loading="lazy"
+            />
+          </div>
+        </div>
+        <div className="dmainrow dmainrow1" id="contact">
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={map}
+              className="img-fluid"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-6">
+            <div className="row rowmarr3">
+              <h1 data-aos="zoom-in-left">Contact</h1>
+            </div>
+            <div className="row rowmarr3">
+              <p data-aos="zoom-in-left">
+                Department of Physics,
+                <br />
+                <br />
+                National Institute Of Technology Patna,
+                <br />
+                Ashok Rajpath, Patna - 800005.
+                <br />
+                <br />
+                Email: head.ph@nitp.ac.in
+                <br />
+                <br />
+                Extn: +91-612-(2371715/2715/2371929/
+                <br />
+                2370419/2370843/2371930) * 163
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="dmainrow">
-        <div className="depabout">
-          <div className="row">
-            <h1 data-aos="zoom-in">About</h1>
-          </div>
-          <div className="row">
-            <p data-aos="zoom-in">
-              The Department of Physics was established in 1924 and since then
-              has been functioning as an important part of the institute. As of
-              now, the Department has eight faculty members and four support
-              staffs at different levels.
-              <br />
-              <br />
-              At present, there are 12 students enrolled in Integrated M. Sc.
-              And 8 students enrolled in the Ph. D. program.
-              <br />
-              <br />
-              The Department is mentored by Prof. K.L.Chopra ,Ex-Director,IIT
-              Kharagpur.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="dmainrow dmainrow1">
-        <div className="col-6 imgcolstyle">
-          <img
-            data-aos="zoom-in"
-            src={mountain}
-            className="img-fluid"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-6">
-          <div className="row rowmarr3">
-            <h1 data-aos="zoom-in-left">Mission</h1>
-          </div>
-          <div className="row rowmarr3">
-            <p data-aos="zoom-in-left">
-              To train the students to be lifelong learners who will contribute
-              to the creation of new knowledge, new technology, and innovation
-              through excellence in research in emerging areas and disciplines.
-              <br />
-              <br />
-              To educate students to be future leaders in science, technology,
-              industry, education and other professions and succeed in a
-              globally competitive environment.
-              <br />
-              <br />
-              To create National and International collaborations for research
-              engagement in strategic areas of research
-              <br />
-              <br />
-              To create National and International collaborations for research
-              engagement in strategic areas of research
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="dmainrow">
-        <div className="col-6">
-          <div className="row rowmarl3">
-            <h1 data-aos="zoom-in-right">Vision</h1>
-          </div>
-          <div className="row rowmarl3">
-            <p data-aos="zoom-in-right">
-              The vision of the Department is to achieve excellence in
-              undergraduate and postgraduate education and research through a
-              synergic combination and thereby lead to development of new
-              knowledge.
-            </p>
-          </div>
-        </div>
-        <div className="col-6 imgcolstyle">
-          <img
-            data-aos="zoom-in"
-            src={lens}
-            className="img-fluid"
-            loading="lazy"
-          />
-        </div>
-      </div>
-      <div className="dmainrow dmainrow1">
-        <div className="col-6 imgcolstyle">
-          <img
-            data-aos="zoom-in"
-            src={map}
-            className="img-fluid"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-6">
-          <div className="row rowmarr3">
-            <h1 data-aos="zoom-in-left">Contact</h1>
-          </div>
-          <div className="row rowmarr3">
-            <p data-aos="zoom-in-left">
-              Department of Physics,
-              <br />
-              <br />
-              National Institute Of Technology Patna,
-              <br />
-              Ashok Rajpath, Patna - 800005.
-              <br />
-              <br />
-              Email: head.ph@nitp.ac.in
-              <br />
-              <br />
-              Extn: +91-612-(2371715/2715/2371929/
-              <br />
-              2370419/2370843/2371930) * 163
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Physicshomepage
