2d7416b5226f579b1645bf45e90a8e7d108ade08
Divya Prakash <medivyaprakash@gmail.com>
2020-10-24 13:21:23 +0530

    terms of use

---
+++ src/pages/terms.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Termsofuse from "../components/termsofuse"
+const Terms = () => (
+  <Layout>
+    <SEO title="Terms of Use" />
+    <Termsofuse />
+  </Layout>
+)
+
+export default Terms
