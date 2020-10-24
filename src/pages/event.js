f129acb5f135056da99fbdbec3f7ab44b8fd7cb5
Divya Prakash <medivyaprakash@gmail.com>
2020-10-24 19:58:44 +0530

    event page add

---
+++ src/pages/event.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Eventpage from "../components/event"
+
+const Event = () => (
+  <Layout>
+    <SEO title="Events" />
+    <Eventpage />
+  </Layout>
+)
+
+export default Event
