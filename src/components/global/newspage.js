eeb3d3f48e20f4a525732ba6b2c90bdfe16f30a6
Divya Prakash <medivyaprakash@gmail.com>
2020-11-25 16:27:56 +0530

    new udate

---
+++ src/components/global/newspage.js
@@ -0,0 +1,172 @@
+import React from "react"
+import "./css/newspage.scss"
+import newspic from "./img/news.jpg"
+
+class Newspage extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="newspage row">
+          <div className="head row">
+            <h1>News</h1>
+          </div>
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="newscard row">
+            <div className="news-img-wrap">
+              <img src={newspic} alt="Hello" loading="lazy"></img>
+            </div>
+            <div className="news-details">
+              <h1>Workshop held in NIT Patna</h1>
+              <p>
+                The hack is only a small part of the competition. The real
+                spirit lies in building your network in the growing tech
+                community and collaborating with some of the finest minds of the
+                nation. Just remember, taking the Hackathons too seriously
+                defeats the purpose. Save the seriousness for pitching to
+                investors. Keeping an open mind will open new doors, help build
+                new relationships and acquire new skills.This is hackNITP.
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Newspage
---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Newspage from "../components/global/newspage"
+const News = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <Newspage />
+  </Layout>
+)
+
+export default News
