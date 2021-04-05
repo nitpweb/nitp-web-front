014c677f416b157ab8af6fa24ab5b41bd1a74c30
Divya Prakash <medivyaprakash@gmail.com>
2021-04-06 00:23:30 +0530

    update gallery

--- src/components/home/gallery.js
+++ src/components/home/gallery.js
@@ -4,30 +4,35 @@ import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
-const TempImg = () => {
+
+ 
+const GalleryComp = () => {
   const data = useStaticQuery(graphql`
     query {
-      main: file(relativePath: { eq: "home/images/600911.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 700) {
-            ...GatsbyImageSharpFluid
+      images: allFile(filter: { relativeDirectory: { eq: "home/images" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
     }
   `)
-  return (
-    <div data-aos="zoom-in" className="tempimg">
-      <Img
-        style={{ width: `100%`, height: `100%` }}
-        fluid={data.main.childImageSharp.fluid}
-        imgStyle={{ width: `100%`, height: `100%`, objectFit: "fill" }}
-      />
-    </div>
-  )
-}
+  const Image = props => {
+    return (
+      <div data-aos="zoom-in" className="tempimg">
+        <Img
+          style={{ width: `100%`, height: `100%` }}
+          fluid={data.images.nodes[props.index].childImageSharp.fluid}
+          imgStyle={{ width: `100%`, height: `100%`, objectFit: "fill" }}
+        />
+      </div>
+    )
+  }
 
-const GalleryComp = () => {
   return (
     <div>
       <div data-aos="zoom-in" data-aos-duration="200" className="gallery-head">
@@ -35,49 +40,49 @@ const GalleryComp = () => {
       </div>
       <Gallery>
         <div className="first-div">
-          <TempImg />
+          <Image index="0" />
         </div>
         <div className="second-div">
           <div className="first-child-div">
-            <TempImg />
+            <Image index="1" />
           </div>
           <div className="second-child-div">
-            <TempImg />
+            <Image index="2" />
           </div>
         </div>
         <div className="third-div">
           <div className="first-child-div">
-            <TempImg />
+            <Image index="3" />
           </div>
           <div className="second-child-div">
-            <TempImg />
+            <Image index="4" />
           </div>
         </div>{" "}
         <div className="fourth-div">
           <div className="first-child-div">
             <div className="short-row-1">
               <div className="first">
-                <TempImg />
+                <Image index="5" />
               </div>
               <div className="second">
-                <TempImg />
+                <Image index="6" />
               </div>
             </div>
             <div className="short-row-2">
               <div className="third">
-                <TempImg />
+                <Image index="7" />
               </div>
               <div className="fourth">
-                <TempImg />
+                <Image index="8" />
               </div>
             </div>
           </div>
           <div className="second-child-div">
-            <TempImg />
+            <Image index="9" />
           </div>
         </div>
         <div className="fifth-div">
-          <TempImg />
+          <Image index="10" />
           <Link to="/gallery">
             <p>See More</p>
           </Link>
