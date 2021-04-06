595c1c3ec954a2dd65d2cd27854c4d00f4595763
Divya Prakash <medivyaprakash@gmail.com>
2021-04-07 00:12:26 +0530

    hss update

---
+++ src/components/departments/hss/hsssyllabus.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Table from "../../table"
+import Course from "./hssCourseStructure"
+import main from "../img/book.svg"
+
+const Mesyllabus = props => {
+  return (
+    <>
+      <div style={{ marginTop: `10vh`, marginBottom: `15vh` }}>
+        <div className="dmainrow dmainrowmain">
+          <div className="col-6">
+            <div className="row rowmarl3">
+              <h1 data-aos="zoom-in-right">Syllabus</h1>
+            </div>
+            <div className="row rowmarl3">
+              <h1 data-aos="zoom-in-right"> -Humanitites & Social Sciences</h1>
+            </div>
+          </div>
+          <div className="col-6 imgcolstyle">
+            <img
+              data-aos="zoom-in"
+              src={main}
+              className="img-fluid"
+              loading="lazy"
+            />
+          </div>
+        </div>
+        <Table>
+          <tr className="syllabus-table-head">
+            <th>Deptt.</th>
+            <th>Sem/Electives</th>
+            <th>Course Code</th>
+            <th>Course Title</th>
+            <th>L</th>
+            <th>T</th>
+            <th>P</th>
+            <th>Credits</th>
+          </tr>
+          {Course.map(elem => (
+            <tr className="syllabus-table-row">
+              <td>{elem.dept}</td>
+              <td>{elem.sem_elect}</td>
+              <td>{elem.course_code}</td>
+              <td>{elem.course_title}</td>
+              <td>{elem.l}</td>
+              <td>{elem.t}</td>
+              <td>{elem.p}</td>
+              <td>{elem.credits}</td>
+            </tr>
+          ))}
+        </Table>
+      </div>
+    </>
+  )
+}
+export default Mesyllabus
