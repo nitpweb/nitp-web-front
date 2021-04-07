445ff464306e2b718ef78d8b86402e21905104ec
Amrit Raj <55765255+dev-amrit460@users.noreply.github.com>
2021-04-07 17:27:12 +0530

    heading overflow fix

--- src/components/home.js
+++ src/components/home.js
@@ -192,7 +192,7 @@ class Home extends React.Component {
                   }
                   var desc = String(news.description).substr(0, 170)
                   if (news.title != "" && news.attachments[0]) {
-                    return <Newscard url={this.link(news.attachments[0].url)} time={d} head={news.title} detail={desc.slice(0,100)} />
+                    return <Newscard url={this.link(news.attachments[0].url)} time={d} head={`${news.title.slice(0,92)}...`} detail={desc.slice(0,100)} />
                   }
                 })
               : null}
