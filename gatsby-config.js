118a13cc00baf187d20eaa53adbff71b5d5a9eaf
Divya Prakash <medivyaprakash@gmail.com>
2021-06-01 02:45:58 +0530

    dynamic route add

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    "gatsby-plugin-use-query-params",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
