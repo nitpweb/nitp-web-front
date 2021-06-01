41ecaec814152cc7fd21fb1c379a4ff9c1c9a782
Divya Prakash <medivyaprakash@gmail.com>
2021-06-01 20:33:40 +0530

    code clean and faculty profile set

---
+++ src/pages/profile.js
@@ -0,0 +1,24 @@
+import React from "react"
+// import Deplayout from "../components/deplayout"
+import SEO from "../components/seo"
+import Facultyprofile from "../components/global/facultyprofile"
+import { useQueryParam, NumberParam, StringParam } from "use-query-params"
+
+const Csefacultyprofilepage = () => {
+  const [id, setNum] = useQueryParam("id", StringParam)
+  console.log(id)
+  if (String(id).endsWith("nitp.ac.in")) {
+    return (
+      <>
+        <SEO title="Faculty Profile" />
+        <Facultyprofile
+          url={`${process.env.GATSBY_API_URL}/api/faculty/${id}`}
+        />
+      </>
+    )
+  } else {
+    return null;
+  }
+}
+
+export default Csefacultyprofilepage
