e74a7b3b86ae8a3cc1f59e24e92bfa7cdd1d3af6
Amrit <55765255+dev-amrit460@users.noreply.github.com>
2021-06-08 00:21:53 +0530

    homepage card fix

--- src/components/home/importantlink.js
+++ src/components/home/importantlink.js
@@ -15,38 +15,62 @@ const Importantlink = () => {
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={chanakya} alt="chanakya" />
             <h3>CHANAKYA</h3>
+            <div className="lead">
+              <p>The student portal for<br/>all the needs of a<br/>student</p>
+            </div>
           </div>
         </a>
         <a href="https://tcfnitp.tech/">
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={corona} alt="corona" />
             <h3>CORONA</h3>
+            <div className="lead">
+              <p>The Annual Techno Cultutal<br/>Festivals oraganised<br/>by the students</p>
+            </div>
           </div>
         </a>
         <a href="https://nss.gov.in/">
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={nss} alt="nss" />
             <h3>NSS</h3>
+            <div className="lead">
+              <p>Aimed at developing student's personality through community service</p>
+            </div>
           </div>
         </a>
-      </div>
-      <div className="link-col">
         <a href="https://josaa.nic.in/webinfocms/Public/home.aspx">
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={josaa} alt="josaa" />
             <h3>JOSAA</h3>
+            <div className="lead">
+              <p>A single platform for taking admission in various academic programs</p>
+            </div>
           </div>
         </a>
+      </div>
+      <div className="link-col">
+        {/* <a href="https://josaa.nic.in/webinfocms/Public/home.aspx">
+          <div data-aos="zoom-in" data-aos-duration="200" className="implink">
+            <img className="linkimg" src={josaa} alt="josaa" />
+            <h3>JOSAA</h3>
+          </div>
+        </a> */}
         <a href="https://www.teqip.in/">
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={teqip} alt="teqip" />
             <h3>TEQIP</h3>
+            <div className="lead">
+              <p>A program to improve learning outcomes and employability of graduate</p>
+            </div>
           </div>
         </a>
-        <a href="http://www.nitp.ac.in/uploads/Orientation_Programe_2018.pdf">
+        <a href={/academics/}>
           <div data-aos="zoom-in" data-aos-duration="200" className="implink">
             <img className="linkimg" src={firstyear} alt="chanakya" />
-            <h3>First Year</h3>
+            <h3>Admissions</h3>
+            <div className="lead">
+              <p>For the newbies willing to take admission in upcoming sessions</p>
+            </div>
           </div>
         </a>
       </div>
