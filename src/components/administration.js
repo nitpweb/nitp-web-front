631307d89fbd289ad698c972114b78a7449b6400
Amrit <55765255+dev-amrit460@users.noreply.github.com>
2021-06-10 13:40:00 +0530

    admin dropdown added

--- src/components/administration.js
+++ src/components/administration.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 // import adminpic from "./administration/img/main.svg"
 import Adcard from "./administration/adcard"
 // import Floatmenu from "./administration/floatmenu"
@@ -6,33 +6,43 @@ import Director from "./director"
 import { AdminStyle } from "./styles/AdminStyle"
 import Navigate from "./global/Navigate"
 import AdministrationData from "./administration/const"
+import { useQueryParam, NumberParam, StringParam } from "use-query-params"
 
 const Administrationpage = () => {
+  const [tab, setNum] = useQueryParam("tab", StringParam)
+
   const [view, setView] = useState([])
-  console.log(view)
   function getView(callback) {
     setView(callback)
   }
-  
+
+  useEffect(() => {
+    AdministrationData.forEach(x => {
+      x.title === tab ? setView(x.data) : ""
+    })
+  }, [tab])
+
   return (
     <>
       <AdminStyle>
         <Navigate data={AdministrationData} callback={getView} />
 
         <div className="mainDiv">
-          {view.length!=0 && view[0].name
-            ? view.map(item => {
-                return (
-                  <div className="col-4">
-                    <Adcard
-                      name={item.name}
-                      designation={item.designation}
-                      type={item.type}
-                    />
-                  </div>
-                )
-              })
-            : <Director id="director" />}
+          {view.length != 0 && view[0].name ? (
+            view.map(item => {
+              return (
+                <div className="col-4">
+                  <Adcard
+                    name={item.name}
+                    designation={item.designation}
+                    type={item.type}
+                  />
+                </div>
+              )
+            })
+          ) : (
+            <Director id="director" />
+          )}
         </div>
       </AdminStyle>
       {/* <Director/> */}
