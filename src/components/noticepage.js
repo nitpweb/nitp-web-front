abbe08444dc9a1838d7d49faab1873e12c9e2434
Divya Prakash <medivyaprakash@gmail.com>
2021-06-10 15:25:01 +0530

    tenders added

---
+++ src/components/noticepage.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import Noticecard from "./home/notice"
+import { CardsStyle } from "./styles/cards"
+
+const Notice = ({type,title}) => {
+  const [notices, setNotices] = useState()
+
+  useEffect(() => {
+    let noticesUrl = `${process.env.GATSBY_API_URL}/api/notice/${type}`
+    axios
+      .get(noticesUrl)
+      .then(res => {
+        setNotices(res.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }, [])
+
+  return (
+    <>
+      <CardsStyle className="row">
+        <div className="card-details-row">
+          <h1>{title}</h1>
+          <div className="fac-card" data-aos="fade-up">
+            {notices && notices.map(notice => {
+              const newtime = new Date().getTime()
+              if (notice.title != "") {
+                return (
+                  <Noticecard
+                    detail={notice.title}
+                    time={notice.openDate}
+                    key={notice.id}
+                    attachments={notice.attachments}
+                    imp={notice.important}
+                    link={
+                      notice.notice_link && JSON.parse(notice.notice_link).url
+                        ? JSON.parse(notice.notice_link).url
+                        : ""
+                    }
+                  />
+                )
+              }
+            })}
+          </div>
+        </div>
+      </CardsStyle>
+    </>
+  )
+}
+
+export default Notice
---
+++ src/pages/tenders.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Noticepage from "../components/noticepage"
+
+const Tenders = () => (
+  <Layout>
+    <SEO title="Tenders" />
+    <Noticepage type="tender" title="Tenders" />
+  </Layout>
+)
+
+export default Tenders
