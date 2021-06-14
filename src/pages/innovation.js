ce8c4415db71afe2f6dcc194b9a5f3d08c39ecb2
Divya Prakash <medivyaprakash@gmail.com>
2021-06-14 16:55:23 +0530

    Rename Innovation.js to innovation.js

---
+++ src/pages/innovation.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Innovationpage from "../components/global/Innovationpage"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { useQueryParam } from "use-query-params"
+import ReadInn from "../components/readinn"
+
+const Innovation = () => {
+  const [id] = useQueryParam("id")
+
+  return (
+    <Layout>
+      <SEO title="Innovation" />
+      <div className="InnovationPage">
+        {id && String(id).length != 0 ? (
+          <ReadInn id={id} />
+        ) : (
+          <Innovationpage />
+        )}
+      </div>
+    </Layout>
+  )
+}
+
+export default Innovation
