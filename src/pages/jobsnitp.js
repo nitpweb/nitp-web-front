24bf5ed52ad035519a9c14e39adb6513a2b6b4c4
Divya Prakash <medivyaprakash@gmail.com>
2021-06-29 03:58:12 +0530

    jobs

---
+++ src/pages/jobsnitp.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Noticepage from "../components/noticepage"
+
+const Jobs = () => (
+  <Layout>
+    <SEO title="Jobs@NITP" />
+    <Noticepage type="facultystaffjob" title="Jobs@NITP" />
+  </Layout>
+)
+
+export default Jobs
