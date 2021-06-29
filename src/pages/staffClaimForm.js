c155fba15b1886bcec27a04bb01861fc343da500
Neeraj Patil <patilneeraj2212@gmail.com>
2021-06-29 23:38:56 +0530

    updates

---
+++ src/pages/staffClaimForm.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import StaffClaimForms from "../components/StaffClaimForms"
+
+const StaffClaim_Forms = () => (
+  <Layout>
+    <SEO title="Faculties" />
+    <StaffClaimForms />
+  </Layout>
+)
+
+export default StaffClaim_Forms
