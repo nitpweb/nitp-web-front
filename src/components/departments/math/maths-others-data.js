1e5b1453f693b5318589e3f8bfa67c610518bb35
Neeraj Patil <patilneeraj2212@gmail.com>
2021-07-02 21:30:41 +0530

    Tables Converted to Object

--- src/components/departments/math/maths-others-data.js
+++ src/components/departments/math/maths-others-data.js
@@ -1,3 +1,68 @@
+const Tables = {
+  projectsRunning: [
+    {
+      "Sl. No": 1,
+      "Name of the projects":
+        "Next Generation E-content of Numerical Methods and Applications",
+      "Name of the faculty": "Prof. S. K. Singh",
+      "Major funding agency": "MHRD",
+      "Completed / Ongoing": "Completed",
+      "Grant Sanctioned (INR in Lakhs)": 5,
+    },
+    {
+      "Sl. No": 2,
+      "Name of the projects":
+        "Numerical Analysis for Singularly Perturbed Parabolic Partial Differential Equations",
+      "Name of the faculty": "Dr. S. Gowrisankar",
+      "Major funding agency": "SERB-DST",
+      "Completed / Ongoing": "Approved",
+      "Grant Sanctioned (INR in Lakhs)": 17,
+    },
+  ],
+  invitedLectures: [
+    {
+      "Sl. No.": 1,
+      Faculty: "Prof. S. Chakrabarty , Maths Department",
+      Institute: "NIT Rourkela",
+    },
+    {
+      "Sl. No.": 2,
+      Faculty: "Prof. A. K. Ganguly , Maths Department",
+      Institute: "Patna University",
+    },
+    {
+      "Sl. No.": 3,
+      Faculty: "Prof. K. N. Rai , Maths Department",
+      Institute: "IIT BHU",
+    },
+    {
+      "Sl. No.": 4,
+      Faculty: "Prof. S. Natesan , Maths Department",
+      Institute: "IIT Guwahati",
+    },
+    {
+      "Sl. No.": 5,
+      Faculty: "Prof. K. N. Rai , Maths Department",
+      Institute: "IIT BHU",
+    },
+    {
+      "Sl. No.": 6,
+      Faculty: "Prof. S. Bhatacharyya , Maths Department",
+      Institute: "IIT Kharagpur",
+    },
+    {
+      "Sl. No.": 7,
+      Faculty: "Prof. Santhana , Maths Department",
+      Institute: "IIT Kanpur",
+    },
+    {
+      "Sl. No.": 8,
+      Faculty: "Prof. Karmeshu , Maths & Computer Science Department",
+      Institute: "J. N. U Delhi",
+    },
+  ],
+}
+
 export const Activities = {
   content: (
     <>
@@ -28,106 +93,42 @@ export const Activities = {
       </div>
       <h2>Externally Funded Projects Running in the Department</h2>
       <table className="table-act">
-        <tr>
-          <td>
-            <b>Sl. No</b>
-          </td>
-          <td>
-            <b>Name of the projects</b>
-          </td>
-          <td>
-            <b>Name of the faculty</b>
-          </td>
-          <td>
-            <b>Major funding agency</b>
-          </td>
-          <td>
-            <b>Completed / Ongoing</b>
-          </td>
-          <td>
-            <b>Grant Sanctioned (INR in Lakhs)</b>
-          </td>
+        <tr className="syllabus-table-head">
+          <th>Sl. No</th>
+          <th>Name of the projects</th>
+          <th>Name of the faculty</th>
+          <th>Major funding agency</th>
+          <th>Completed / Ongoing</th>
+          <th>Grant Sanctioned (INR in Lakhs)</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>
-            Next Generation E-content of Numerical Methods and Applications
-          </td>
-          <td>Prof. S. K. Singh</td>
-          <td>MHRD</td>
-          <td>Completed</td>
-          <td>5</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>
-            Numerical Analysis for Singularly Perturbed Parabolic Partial
-            Differential Equations
-          </td>
-          <td>Dr. S. Gowrisankar</td>
-          <td>SERB-DST</td>
-          <td>Approved</td>
-          <td>17</td>
-        </tr>
+        {Tables.projectsRunning.map((row, idx) => (
+          <tr key={idx}>
+            <td>{row["Sl. No"]}</td>
+            <td>{row["Name of the projects"]}</td>
+            <td>{row["Name of the faculty"]}</td>
+            <td>{row["Major funding agency"]}</td>
+            <td>{row["Completed / Ongoing"]}</td>
+            <td>{row["Grant Sanctioned (INR in Lakhs)"]}</td>
+          </tr>
+        ))}
       </table>
       <div className="row ">
         <h2>Invited Lectures</h2>
       </div>
       <div>
-        {" "}
         <table className="table-act">
-          <tr>
-            <td>
-              <b>Sl. No.</b>
-            </td>
-            <td>
-              <b>Faculty</b>
-            </td>
-            <td>
-              <b>Institute</b>
-            </td>
+          <tr className="syllabus-table-head">
+            <th>Sl. No.</th>
+            <th>Faculty</th>
+            <th>Institute</th>
           </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Prof. S. Chakrabarty , Maths Department </td>
-            <td>NIT Rourkela</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Prof. A. K. Ganguly , Maths Department </td>
-            <td>Patna University</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Prof. K. N. Rai , Maths Department </td>
-            <td>IIT BHU</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Prof. S. Natesan , Maths Department </td>
-            <td>IIT Guwahati </td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Prof. K. N. Rai , Maths Department </td>
-            <td>IIT BHU</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>Prof. S. Bhatacharyya , Maths Department </td>
-            <td>IIT Kharagpur </td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>Prof. Santhana , Maths Department </td>
-            <td>IIT Kanpur</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>Prof. Karmeshu , Maths & Computer Science Department </td>
-            <td>J. N. U Delhi</td>
-          </tr>
+          {Tables.invitedLectures.map((row, idx) => (
+            <tr key={idx}>
+              <td>{row["Sl. No."]}</td>
+              <td>{row.Faculty}</td>
+              <td>{row.Institute}</td>
+            </tr>
+          ))}
         </table>
       </div>
     </>
