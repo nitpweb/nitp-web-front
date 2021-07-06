de746b39ec82320fb1cf776354882a7cd11f6a0e
Amrit <55765255+dev-amrit460@users.noreply.github.com>
2021-07-06 13:58:42 +0530

    revert change and added new

--- src/pages/director.js
+++ src/pages/director.js
@@ -2,10 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Directorpage from "../components/director"
+import { TabPage } from "../components/styles/tabpage"
 const Director = () => (
   <Layout>
     <SEO title="Director's Desk" />
+    <TabPage>
+    <div className="mainDiv">
     <Directorpage />
+    </div>
+    </TabPage>
   </Layout>
 )
 
