19588fd5b32361e28183cd468f0bb60ff2413e30
Neeraj Patil <patilneeraj2212@gmail.com>
2021-07-12 00:49:24 +0530

    Updated Home Page Carousel

---
+++ src/components/Slider/BtnSlider.js
@@ -0,0 +1,14 @@
+import React from "react"
+import leftArrow from "./icons/left-arrow.svg"
+import rightArrow from "./icons/right-arrow.svg"
+
+export default function BtnSlider({ direction, moveSlide }) {
+  return (
+    <button
+      onClick={moveSlide}
+      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+    >
+      <img src={direction === "next" ? rightArrow : leftArrow} />
+    </button>
+  )
+}
