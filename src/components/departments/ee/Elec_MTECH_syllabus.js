dae99119fa48b202737feb96eed884efc587d554
Divya Prakash <medivyaprakash@gmail.com>
2021-08-03 16:02:42 +0530

    update

--- src/components/departments/ee/Elec_MTECH_syllabus.js
+++ src/components/departments/ee/Elec_MTECH_syllabus.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const EEMtechSyllabusControl = [
   {
     prog: `M.Tech-EE`,
--- src/components/departments/ee/Elelc_phd.js
+++ src/components/departments/ee/Elelc_phd.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const EEPhdCourse = [
   {
     type: `Mandatory Course`,
