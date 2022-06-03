f166941557e932da81efef46cc52b50b1290aba9
Pragati1122 <pragativerma2001@gmail.com>
2022-06-03 18:40:10 +0530

    Updated gallery images and homepage bg images

--- src/components/home/video.js
+++ src/components/home/video.js
@@ -1,9 +1,12 @@
 import React from 'react'
+// import video1 from "./video/demoVideo.mp4"
+import logo from './video/load.gif'
 import { VideoStyle } from './styles'
 function Video() {
   return (
     <VideoStyle>
-        I am blue color
+      {/* <video src={video1} width="600" height="300" controls="controls" autoplay="true" /> */}
+      <img src={logo} alt="loading..." />
     </VideoStyle>
   )
 }
