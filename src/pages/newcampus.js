efe55a7d97db97ef1562ea6f4efe925696ad9515
Divya Prakash <medivyaprakash@gmail.com>
2022-07-25 22:29:24 +0530

    New Campus added

---
+++ src/pages/newcampus.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Noticepage from "../components/noticepage"
+
+const NewCampus = () => (
+  <Layout>
+    <SEO title="Jobs@NITP" />
+    <Noticepage type="newcampus" title="New Campus Notices" />
+  </Layout>
+)
+
+export default NewCampus
