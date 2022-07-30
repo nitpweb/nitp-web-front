2dd08e70d659cb4ba51aee68d3078d5f3d35578e
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2022-07-30 11:29:48 +0530

    Syllabus Tab view added

--- src/components/departments/cse/csesyllabus.js
+++ src/components/departments/cse/csesyllabus.js
@@ -1,13 +1,95 @@
-import React from "react"
-import Table from "../../table"
-import Course from "./cse BTech CourseStructure"
+import React, { useEffect, useState } from "react"
+import Courseug from "./cse BTech CourseStructure"
+import Coursephd from "./phd course"
 import main from "../img/book.svg"
 import { PageLayout } from "../../styles/pagelayout"
 
+import { TabPage } from "../../styles/tabpage"
+import Table from "../../table"
+
 const Csesyllabus = props => {
+    const [course, setCourse] = useState("programmesug")
+  const [syllabus, setSyllabus] = useState(Courseug)
+
+  useEffect(() => {
+    
+  }, [course])
+  
   return (
     <>
-      <PageLayout style={{ marginTop: `10vh`, marginBottom: `15vh` }}>
+    <TabPage>
+          <div className="mainDiv" style={{width: "90vw"}}>
+            <div className="row rowmarl3">
+              <div className="digital">
+                <h1 style={{ marginBottom: `1rem` }}>Syllabus</h1>
+                <h2 data-aos="zoom-in-right">
+                    {" "}
+                    -Computer Science and Engineering
+                  </h2>
+
+                <div className="probutton">
+                  <button
+                    onClick={() => {
+                      setCourse("programmesug")
+                      setSyllabus(Courseug)
+                    }}
+                    className={course == "programmesug" ? "btnactive" : ""}
+                  >
+                    UG Courses
+                  </button>
+                  {/* <button
+                    onClick={() => {
+                      setCourse("programmespg")
+                      setSyllabus(Coursepg)
+                    }}
+                    className={course == "programmespg" ? "btnactive" : ""}
+                  >
+                    PG (M.tech/MURP) Courses
+                  </button> */}
+                  <button
+                    onClick={() => {
+                      setCourse("programmesphd")
+                      setSyllabus(Coursephd)
+                    }}
+                    className={course == "programmesphd" ? "btnactive" : ""}
+                  >
+                    PhD Courses
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <Table style={{width:"100%", marginTop:"20px"}}>
+              <br />
+              <tr className="syllabus-table-head">
+                <th>Dept.</th>
+                <th>Sem/Electives</th>
+                <th>Course Code</th>
+                <th>Course Title</th>
+                <th>L</th>
+                <th>T</th>
+                <th>P</th>
+                <th>Credits</th>
+              </tr>
+              {syllabus.map(elem => (
+                <tr className="syllabus-table-row">
+                  <td>{elem.dept}</td>
+                  <td>{elem.sem_elect}</td>
+                  <td>{elem.coursecode}</td>
+                  <td>
+                    <a href={elem.url}>{elem.coursetitle}</a>
+                  </td>
+                  <td>{elem.l}</td>
+                  <td>{elem.t}</td>
+                  <td>{elem.p}</td>
+                  <td>{elem.credits}</td>
+                </tr>
+              ))}
+            </Table>
+          </div>
+        </TabPage>
+        
+      {/* <PageLayout style={{ marginTop: `10vh`, marginBottom: `15vh` }}>
         <div className="syllabus-page">
           <div className="layoutrow layoutrowmain syllabus-page-head">
             <div className="col-6">
@@ -49,8 +131,9 @@ const Csesyllabus = props => {
             ))}
           </Table>
         </div>
-      </PageLayout>
+      </PageLayout> */}
     </>
   )
 }
+
 export default Csesyllabus
