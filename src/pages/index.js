c0f6ec28b346fa3d83d49de4ab6d5d0e0d6e6b57
Divya Prakash <medivyaprakash@gmail.com>
2022-08-16 22:59:16 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from "react"
 import Home from "../components/home"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Initial_popup from "../components/home/initial_popup"
 
 const IndexPage = () => (
   <>
-    <Initial_popup/>
     <Layout>
       <SEO title="NIT Patna" />
       <Home />
