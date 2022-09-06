7c5940670e9e919a6d3490437693a3af2a195d12
Divya Prakash <medivyaprakash@gmail.com>
2022-09-06 14:56:34 +0530

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import React from "react"
+import React,{useEffect} from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { navigate } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    setInterval(5000);
+    navigate("/");
+  }, [])
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You requested a URL which does not exist. You are being redirected to homepage in 5s.</p>
+    </Layout >
+  )
+}
 
 export default NotFoundPage
