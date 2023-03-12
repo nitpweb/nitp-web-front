1fa1e092b368db57b4efd27a23bf856f79d2bad1
Shivam Jha <jha.shivam2001@gmail.com>
2023-03-12 13:23:16 +0530

    Add: Format for official documents

--- src/components/academics/acadData.js
+++ src/components/academics/acadData.js
@@ -582,16 +582,19 @@ const Format = [
           "http://web.nitp.ac.in/uploads22/DRCC_Adjustment_SOP.pdf",
       },
       {
-        para:
-          "5-Procedure for issue of Demand Letter for Bank/DRCC student credit card loan.",
-        link: "http://web.nitp.ac.in/uploads20/demand_letter_sop.pdf",
+        para: "5-Application for Demand Letter.",
+        link: "http://web.nitp.ac.in/uploads23/Application%20for%20Demand%20Letter.pdf",
       },
       {
-        para: "6-Procedure for Educational verification of Students.",
-        link: "http://web.nitp.ac.in/uploads20/verification_sop.pdf",
+        para: "6-Application for Hostel & Mess Fee Receipt.",
+        link: "http://web.nitp.ac.in/uploads23/Application%20for%20Hostel%20&%20Mess%20Fee%20Receipt.pdf",
       },
       {
-        para: "7-SOP for Cancellation of Admission",
+        para: "7-SOP for Educational Verification of Students.",
+        link: "http://web.nitp.ac.in/uploads23/Educational%20Verification_SOP.pdf",
+      },
+      {
+        para: "8-SOP for Cancellation of Admission",
         link: "http://web.nitp.ac.in/uploads22/Application for Cancellation of Admission.pdf",
       },
     ],
