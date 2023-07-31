1168316f5f7817d3875b950a973aa04ccaa7fa7a
akshaydeepak <akshaydeepak@users.noreply.github.com>
2023-07-31 15:33:56 +0530

    commit message

--- src/components/departments/cse/cse-others-data.js
+++ src/components/departments/cse/cse-others-data.js
@@ -310,7 +310,7 @@ export const TimeTable = [
     link:"http://web.nitp.ac.in/uploads23/FirstYearTimeTable_AllSubjectsByCSED_July-Dec2022.pdf",
   },*/
  {
-  title: "Click here for the time table of CSED (all semesters)",
+  title: "Click here for the time table of CSED (for all semesters)",
   link:
    "https://www.dropbox.com/sh/76m1ukfspklwuip/AABfILH6jGMDc9F8y4BefvRaa?dl=0",
  },
